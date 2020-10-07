b4be483ea8910f00f5405214ef62874afb4605e5
WaelBerro <wael.berro@isen.yncrea.fr>
2020-10-07 14:09:07 +0200

    Test

--- TP4/Ex1/Ex1.c
+++ TP4/Ex1/Ex1.c
@@ -12,10 +12,15 @@ int main()
 	duree.heure = 0;
 	duree.minute = 45;
 	heurefin.minute = duree.minute + heuredebut.minute;
+	heurefin.heure = duree.heure + heuredebut.heure;
 	if (heurefin.minute > 59)
 	{
 		heurefin.minute = heurefin.minute - 60;
 		heurefin.heure = heurefin.heure + 1;
 	}
-
+	if (heurefin.heure > 24)
+	{
+		heurefin.heure = heurefin.heure - 24;
+	}
+	printf("voici la nouvelle heure %d:%d", heurefin.heure, huerefin.minute);
 }
